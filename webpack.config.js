12c4312d29e4d446c12dd78cfd25b0d9bd72d1f3
adev05 <andrew.nasibulin@mail.ru>
2024-11-26 18:50:56 +0300

    deploy

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
   output: {
     path: buildPath,
     filename: 'bundle.js',
-    publicPath: '/',
+    publicPath: './',
   },
   devtool: 'source-map',
   target: !isProd ? 'web' : 'browserslist',
